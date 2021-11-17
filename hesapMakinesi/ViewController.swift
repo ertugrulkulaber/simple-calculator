933cc4e4de8a96f1d36ad5d5e1293275fe3385ae
ertugrulkulaber <92637137+ertugrulkulaber@users.noreply.github.com>
2021-11-17 15:46:39 +0300

    simple calculator

--- hesapMakinesi/ViewController.swift
+++ hesapMakinesi/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onenumberText: UITextField!
+    
+    @IBOutlet weak var twoNumberText: UITextField!
+    
+    @IBOutlet weak var sonucLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func toplamaButton(_ sender: Any) {
+        
+        
+        if let oneNumber = Int (onenumberText.text!){
+         if   let twoNumber = Int (twoNumberText.text!){
+                
+             let sonuc = oneNumber + twoNumber
+               sonucLabel.text = String (sonuc)
+         }
+        } else {
+            sonucLabel.text = "Lütfen Bi Sayı Giriniz"
+        }
+    }
+    
+    @IBAction func cikarmaButton(_ sender: Any) {
+        let oneNumber = Int (onenumberText.text!)!
+        let twoNumber = Int (twoNumberText.text!)!
+        
+      let sonuc = oneNumber - twoNumber
+        sonucLabel.text = String (sonuc)
+    }
+    
+    @IBAction func bolmeButton(_ sender: Any) {
+        let oneNumber = Int (onenumberText.text!)!
+        let twoNumber = Int (twoNumberText.text!)!
+        
+      let sonuc = oneNumber / twoNumber
+        sonucLabel.text = String (sonuc)
+    }
+    @IBAction func carpmaButton(_ sender: Any) {
+        let oneNumber = Int (onenumberText.text!)!
+        let twoNumber = Int (twoNumberText.text!)!
+        
+      let sonuc = oneNumber * twoNumber
+        sonucLabel.text = String (sonuc)
+    }
+    
 }
 
